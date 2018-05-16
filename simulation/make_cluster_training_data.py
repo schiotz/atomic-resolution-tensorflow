fed3e4e5a660e21a8e8bd0ea7ecab08fd7e8ab88
Jakob Schiotz <schiotz@fysik.dtu.dk>
2018-05-16 13:10:23 +0200

    Update cluster training data script to include column positions and heights.

--- simulation/make_cluster_training_data.py
+++ simulation/make_cluster_training_data.py
@@ -5,6 +5,7 @@ import numpy as np
 from pyqstem.util import atoms_plot
 from labels import project_positions,create_label
 import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import fcluster, linkage
 from pyqstem import PyQSTEM
 from pyqstem.imaging import CTF
 #from IPython.display import clear_output
@@ -15,11 +16,11 @@ import os
 import time
 
 # Parameters
-N=360
+N=424
 sampling=24.48/2048*10
 L=sampling*N
-#num_examples=1000   # Training
-num_examples=146   # Catch up
+num_examples=1000   # Training
+#num_examples=146   # Catch up
 #num_examples=500     # Test
 num_classes=1
 
@@ -44,7 +45,7 @@ qstem=PyQSTEM('TEM')
 # Create the models
 before = time.time()
 for i in range(num_examples):
-    radius=4+np.random.rand()*10
+    radius=5+np.random.rand()*9
     
     lengths100=np.random.uniform(radius,radius+.2*radius,6)
     lengths111=np.random.uniform(radius-.2*radius,radius,8)
@@ -56,6 +57,11 @@ for i in range(num_examples):
     atoms=rc.get_cluster('Au')
     
     atoms.rotate(v='y',a=np.pi/4.)
+
+    # Find the atomic columns
+    clusters = fcluster(linkage(positions[:,:2]), 1, criterion='distance')
+    unique,indices=np.unique(clusters, return_index=True)
+    
     atoms.rotate(v='z',a=np.random.random()*2*np.pi)
 
     omega=np.random.random()*2*np.pi
@@ -75,8 +81,13 @@ for i in range(num_examples):
     ty=(L-size[1]-5)*(np.random.rand()-.5)
 
     atoms.translate((tx,ty,0))
+
+    # Find the positions of the columns and the number of atoms per column.
+    positions=atoms.get_positions()[:,:2]
+    sites = np.array([np.mean(positions[clusters==u],axis=0) for u in unique])
+    heights = np.array([np.sum(clusters==u) for u in unique])
     
-    positions,counts=project_positions(atoms,distance=.8,return_counts=True)
+    #positions,counts=project_positions(atoms,distance=.8,return_counts=True)
     
     model = atoms
 
@@ -91,7 +102,8 @@ for i in range(num_examples):
     
     positions,counts=project_positions(model,distance=.8,return_counts=True)
     label=create_label(positions/sampling,(N,)*2,width=12,num_classes=num_classes,null_class=False)
-    
+
+    np.savez('{0}/points/points_{1:04d}.npz'.format(dir_name,first_number+i), sites=sites, heights=heights)
     np.save('{0}/label/label_{1:04d}.npy'.format(dir_name,first_number+i),label)
     wave.save('{0}/wave/wave_{1:04d}.npz'.format(dir_name,first_number+i))
     write('{0}/model/model_{1:04d}.cfg'.format(dir_name,first_number+i),model)
